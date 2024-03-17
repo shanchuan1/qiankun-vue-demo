44dc4822b1f93ac6db62ab52f7214dfa81c77027
zhongqifeng <1121021693@qq.com>
2024-03-17 23:34:19 +0800

    feat: 代理子应用地址

--- app-vue-history/vue.config.js
+++ app-vue-history/vue.config.js
@@ -7,6 +7,13 @@ module.exports = {
     },
   },
   chainWebpack: (config) => {
+    // 在 HTMLWebpackPlugin 插件生成的 HTML 中为 script 标签的 src 属性补全路径
+    config.plugin('html').tap(args => {
+      args[0].chunksSortMode = 'none'; // 保持 chunk 的顺序不变
+      args[0].filename = 'index.html'; // 更改生成的 HTML 文件名
+      return args;
+    });
+    
     config.module
       .rule('fonts')
       .test(/.(ttf|otf|eot|woff|woff2)$/)
@@ -23,4 +30,5 @@ module.exports = {
       jsonpFunction: `webpackJsonp_${name}`,
     },
   },
+  
 };
